4578c8fe726cbee790e57f98eed5070abdc6c8d7
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2012-06-04 13:37:33 -0400

    Lunch time break... moving to Python; currently reading out the lines that contain the hyperlinks to follow to get the download links.

--- cbs_radio.pm
+++ cbs_radio.pm
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
 
-use LWP::Simple;
-
-$url_list = "http://www.cbsrmt.com/?s=synopsis&y=all";
-$url_dl = "http://www.cbsrmt.com/mp3/CBS%20Radio%20Mystery%20Theater%2074-01-06%20e0001%20The%20Old%20Ones%20Are%20Hard%20to%20Kill.mp3"
+#use LWP::Simple;
+#
+#$url_list = "http://www.cbsrmt.com/?s=synopsis&y=all";
+#$url_dl = "http://www.cbsrmt.com/mp3/CBS%20Radio%20Mystery%20Theater%2074-01-06%20e0001/%20The%20Old%20Ones%20Are%20Hard%20to%20Kill.mp3"
 $list = "list.html";
 
-getstore($url_list,$list);
+#getstore($url_list,$list);
 
 open LIST, "<", $list or die "unable to open $list";
 foreach $line (<LIST>){
-    if ($line =~ m/<td><a href='.\/episode/){
-        print $line;
+    if ($line =~ m/<td><a href='.\/episode-/){
+        $line =~ s/<td><a.+?>//;
+        $line =~ s/<.+$//;
+        @dl_url_part = split ' ',$line;
+        foreach (@dl_url_part){
+            print $_,"\%20";
+        }
+        print "\n";
         die;
     }
 }
--- cbs_radio.py
+++ cbs_radio.py
@@ -1,2 +1,13 @@
-import urllib
-urllib.urlretrieve("http://www.cbsrmt.com/?s=synopsis&y=all","list.html");
+import urllib,os.path,re
+top_file = "top.html"
+top_url = "http://www.cbsrmt.com/?s=synopsis&y=all"
+if not(os.path.exists(top_file)):
+    print "File "+top_file+" not found...\n","Fetching it from "+top_url
+    urllib.urlretrieve(top_url,top_file)
+else:
+    print "File "+top_file+" already exists... not fetching"
+top = open(top_file,"r")
+for line in top:
+    if re.match(".+?<td><a href='./episode-.+?</td>.*",line):
+        print line
+top.close()
