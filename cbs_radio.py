37aadd08f15c4cd90b9db9bb2237710ea22c97be
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2012-06-04 15:36:44 -0400

    All first level links parsed and stored in a list

--- cbs_radio.py
+++ cbs_radio.py
@@ -1,13 +1,20 @@
 import urllib,os.path,re
-top_file = "top.html"
-top_url = "http://www.cbsrmt.com/?s=synopsis&y=all"
+top_file = 'top.html'
+top_url = 'http://www.cbsrmt.com/?s=synopsis&y=all'
+number_to_find = 1399
 if not(os.path.exists(top_file)):
-    print "File "+top_file+" not found...\n","Fetching it from "+top_url
+    print 'file',top_file,'not found...\n','Fetching it from',top_url
     urllib.urlretrieve(top_url,top_file)
 else:
-    print "File "+top_file+" already exists... not fetching"
-top = open(top_file,"r")
+    print 'file',top_file,'already exists... not fetching'
+top = open(top_file,'r')
+pages = []
+count = 0
 for line in top:
-    if re.match(".+?<td><a href='./episode-.+?</td>.*",line):
-        print line
+    if re.match('.+?<td><a href=\'./episode-.+?</td>.*',line):
+        page = re.findall('\'.+?\'',line) # only need one value, though...
+        page[0] = re.sub('\'[\.]*','',page[0]);
+        pages.append(page[0])
+        count += 1
+print 'found',count,'subpages'
 top.close()
